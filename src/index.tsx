43c75b6f9eb64b8f8dd0ec02b22f439cb96d1d02
savbace <7579898+savbace@users.noreply.github.com>
2023-11-29 09:55:56 +0200

    move about

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Movies from './features/Movies/Movies';
-import About from './About';
+import About from './features/About/About';
 import { Provider } from 'react-redux';
 import store from './store';
 
