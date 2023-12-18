2752867d8d69fc5d6d7b2c787cb80d6cf5847907
savbace <7579898+savbace@users.noreply.github.com>
2023-12-18 23:39:05 +0200

    naive fetch: now playing

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -4,16 +4,52 @@ import { connect } from "react-redux";
 import { RootState } from "../../store";
 import { Movie } from "../../reducers/movies";
 import MovieCard from "./MovieCard";
+import { useEffect, useState } from "react";
+import configuration from "../../configuration";
+
+async function getNowPlaying() {
+    var headers = new Headers();
+    headers.append("Accept", "application/json");
+    headers.append("Authorization", `Bearer ${configuration.apiToken}`);
+
+    var requestOptions = {
+        method: "GET",
+        headers: headers
+    };
+
+    const response = await fetch(`${configuration.apiUrl}/3/movie/now_playing`, requestOptions);
+    const value = await response.json();
+    return value;
+}
 
 interface Props {
     movies: Movie[];
 }
 
 function Movies({ movies }: Props) {
+    const [moviesTemp, setMoviesTemp] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const value = await getNowPlaying();
+            const nowPlaying: Movie[] = value.results.map((movie: Movie) => ({
+                id: movie.id,
+                title: movie.title,
+                overview: movie.overview,
+                popularity: movie.popularity
+            }));
+            
+            setMoviesTemp(nowPlaying);
+        };
+
+        fetchMovies()
+            .catch(console.error);
+    }, []);
+
     return (
         <section>
             <div className={styles.list}>
-                {movies.map(m => (
+                {moviesTemp.map(m => (
                     <MovieCard
                         key={m.id}
                         id={m.id}
