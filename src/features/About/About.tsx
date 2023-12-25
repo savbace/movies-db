fb24fa9786f459d47d5515ea7dba99ba0624c6d7
savbace <7579898+savbace@users.noreply.github.com>
2023-12-26 01:03:48 +0200

    useRef: interval

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -1,13 +1,33 @@
 import { Container, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
 function About() {
-    return (
-        <Container sx={{ py: 8 }} maxWidth="md">
-            <Typography variant="h5" align="center">
-                Coming soon...
-            </Typography>
-        </Container>
-    );
+  const intervalRef = useRef<any>(null);
+  const [countdown, setCountdown] = useState(9);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCountdown((value) => value - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      clearInterval(intervalRef.current);
+    }
+  }, [countdown]);
+
+  return (
+    <Container sx={{ py: 8 }} maxWidth="md">
+      <Typography variant="h5" align="center">
+        Coming soon: {countdown}.
+      </Typography>
+    </Container>
+  );
 }
 
 export default About;
